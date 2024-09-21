0b726c75077b40687da4f9b3f866fbfe00ea1a5c
Fareed Ahmed <54204949+Fareedsea@users.noreply.github.com>
2024-09-21 15:22:40 +0500

    Update Files

---
+++ page.tsx
@@ -0,0 +1,13 @@
+import { Nav } from "../navbar/nav"
+
+function Contactus() {
+     return (
+      <div class="bg-green-300 h-screen flex justify-center items-center flex-col gap-5">
+        <Nav/>
+        <br />
+        <h1 class="text-5xl font-bold underline">Contact Us Page</h1>
+      </div>
+      
+    );
+  }
+  export default Contactus 
